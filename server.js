9ac4998fad1202ddf09c649c392690df523783b6
2110Abhishek <agchoudhari2110@gmail.com>
2025-09-02 19:17:11 +0530

    Backend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const schoolRoutes = require("./routes/schoolRoutes");
 app.use("/api/schools", schoolRoutes);
 
 // Start server
-const PORT = 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
