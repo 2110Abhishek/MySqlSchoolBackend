c2e22f768013478bb82f7bb1688cb7137e429680
2110Abhishek <agchoudhari2110@gmail.com>
2025-09-02 20:10:33 +0530

    Backend

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
+// backend/config/db.js
+
 const mysql = require("mysql2");
+require("dotenv").config(); // Load .env variables if running locally
 
 const db = mysql.createConnection({
-  host: "centerbeam.proxy.rlwy.net", // Railway public host
-  port: 21735,                        // Railway port
-  user: "root",                       // Railway user
-  password: "OPcTmwNWGCwEWHbVfQvAFVCZlzUvHIFr", // Railway password
-  database: "railway"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
